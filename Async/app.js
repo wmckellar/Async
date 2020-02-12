4cea11b376fb35b52b4acd16ae6c3b46439f5489
wmckellar <59664354+wmckellar@users.noreply.github.com>
2020-02-12 16:03:10 -0600

    Add files via upload

---
+++ Async/app.js
@@ -0,0 +1,53 @@
+// setTimeout (function mine (message) {
+//     alert("hello");
+// }, 2000);
+
+// setTimeout (function getWords () {
+//     console.log("hello");
+//     console.log(("hi"), 2000);
+//     console.log(("howdie"), 3000);
+//     console.log(("final"), 1000)
+// });
+
+// function done() {
+//     console.log("Job's done.")
+// }
+
+// done();
+
+// function countdown(num, CallBack) {
+//     setTimeout(function () {
+
+//         if (num > 0) {
+//             console.log(num);
+//             countdown(num - 1, CallBack);
+//         }
+//         else {
+//             CallBack ();
+//         }
+//         }, 1000
+//             );
+// }
+
+// function isDone () {
+//     console.log( "Everything is done!");
+// }
+
+// countdown (5, isDone);
+
+let lunchTime = true;
+
+function orderMeSomeFood() {
+    return new Promise((resolve, reject) => {
+        if (lunchTime == true) {
+            let meal = {lunch: "pizza", drink: "root beer"};
+            resolve (console.log(meal));
+        }
+        else {
+        err = "No soup for you!"
+        reject (console.log(err));
+        }
+    });
+};
+
+orderMeSomeFood ();
